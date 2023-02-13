18b2e012fac0a8fb87b012a65641151f60d2a965
Aura <ax.pink@gmail.com>
2023-02-13 14:57:17 -0500

    updated

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import './navbar.scss'
+
+const Navbar = () => {
+    return (
+        <section class="stage">
+        <div id="home" class="div"><p id="homep" class="p">Home</p></div>
+        <div class="div" id="about"><p class="p">Outfit Creator</p>
+        </div>
+        <div class="div" id="contact"><p class="p">Outfit Map</p>
+        </div>
+        <div class="div" id="gallery"><p class="p">LogIn</p>
+        </div>
+      </section>
+    )
+}
+
+export default Navbar
---
+++ src/components/OutfitCreator.jsx
@@ -0,0 +1,23 @@
+import './OutfitCreator.scss'
+
+const OutfitCreator = () => {
+    return (
+    <section id="slider" class="section">
+    <div class="slider">
+      <div class="slider__container">
+        <div class="tops">
+          <div class="slider__content grid">
+            <div class="column">
+                </div>
+                    <div class="column">
+                        <img src="https://images.urbndata.com/is/image/FreePeople/78987070_066_a/?$a15-pdp-detail-shot$&fit=constrain&fmt=webp&qlt=80&wid=1080" class="tops-img"></img>
+                    </div>
+                </div>
+            </div>
+        </div>
+     </div>
+    </section>
+ )
+}
+
+export default OutfitCreator 
