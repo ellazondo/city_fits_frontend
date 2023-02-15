812ab019f8cf30a25acd3833f251fa6a3974a317
Aura <ax.pink@gmail.com>
2023-02-15 17:26:19 -0500

    uptaded form

--- src/components/OutfitCreator/OutfitForm/OutfitForm.jsx
+++ src/components/OutfitCreator/OutfitForm/OutfitForm.jsx
@@ -2,59 +2,58 @@ import './OutfitForm.scss'
 import { useState } from "react"
 
 export default function OutfitForm() {
-  // const { link, name } = product;
-  // const [outfitTop, setOutfitTop] = useState("")
-  // const [outfitBottom, setOutfitBottom] = useState("")
-  // const [outfitShoes, setOutfitShoes] = useState("")
-  // const [newOutfitName, setNewOutfitName] = useState("");
-
-  // function handleSubmit(e) {
-  //   e.preventDefault();
-  //   fetch("http://localhost:3000/outfit_products", {
-  //     method: "POST",
-  //     headers: {
-  //       "Content-Type": "application/json",
-  //     },
-  //     body: JSON.stringify({
-  //       outfitTop: outfitTop,
-  //       outfitBottom: outfitBottom,
-  //       outfitShoes: outfitShoes,
-  //       name: name
-  //     }),
-  //   })
-  //     .then((r) => r.json())
-  //     .then((newOutfit) => onAddOutfit(newOutfit));
-  // }
+  
 
   return (
     <div className="new-outfit-form">
-      <h2>Create Outfit</h2>
-      <div>
-        <div>
-          <h2>Top</h2>
-          <span>
-        <picture ><img className="productcard" src="https://images.urbndata.com/is/image/FreePeople/80826902_266_a/?$a15-pdp-detail-shot$&fit=constrain&fmt=webp&qlt=80&wid=720" /></picture>
+      <div className='saved-outfit'>
+        <div className='box'>
+        <span className='imgBox'>
+          <picture ><img className="productcard-form" src="https://images.urbndata.com/is/image/FreePeople/80826902_266_a/?$a15-pdp-detail-shot$&fit=constrain&fmt=webp&qlt=80&wid=720" /></picture>
         </span>
-        
-          <h2>bottom</h2>
-          <span>
-        <picture ><img className="productcard" src="https://images.urbndata.com/is/image/FreePeople/79292488_060_c/?$a15-pdp-detail-shot$&fit=constrain&fmt=webp&qlt=80&wid=640" /></picture>
+        </div>
+            
+            <div className='box'>
+        <span className='imgBox'>
+          <picture ><img className="productcard-form" src="https://images.urbndata.com/is/image/FreePeople/82717968_000_a/?$a15-pdp-detail-shot$&fit=constrain&fmt=webp&qlt=80&wid=1080" /></picture>
         </span>
-        
-          <h2>Shoes</h2>
-          <span>
-        <picture ><img className="productcard" src="https://images.urbndata.com/is/image/FreePeople/69311884_009_b/?$a15-pdp-detail-shot$&fit=constrain&fmt=webp&qlt=80&wid=640" /></picture>
+        </div>
+            
+
+            <div className='box'>
+        <span className='imgBox'>
+          <picture ><img className="productcard-form" src="https://images.urbndata.com/is/image/FreePeople/56313224_111_c/?$a15-pdp-detail-shot$&fit=constrain&fmt=webp&qlt=80&wid=1080"></img></picture>						
         </span>
         </div>
       </div>
-      <form>
+
+        <h2 id='form-title'>CREATE YOUR OUTFIT</h2>
+
+      <div id='form-div'>
+      <form id='outfit-form'>
         <input
           type="text"
           name="name"
           placeholder="Outfit name"
         />
-        <button type="save">Save Outfit</button>
+         <input
+          type="text"
+          name="name"
+          placeholder="User name"
+        />
+        <button id='form-btn' type="save">Save Outfit</button>
+        <p href="#">Don't have an account? Sign In</p>
       </form>
+
+      
+  <div class="drops">
+    <div class="drop drop-1"></div>
+    <div class="drop drop-2"></div>
+    <div class="drop drop-3"></div>
+    <div class="drop drop-4"></div>
+    <div class="drop drop-5"></div>
+  </div>
+    </div>
     </div>
   );
 }
