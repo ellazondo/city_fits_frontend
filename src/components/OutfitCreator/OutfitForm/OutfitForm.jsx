1b6099eb96a11376134d42dcd0eba7abc95656d5
Aura <ax.pink@gmail.com>
2023-02-16 15:49:30 -0500

    Updated form

--- src/components/OutfitCreator/OutfitForm/OutfitForm.jsx
+++ src/components/OutfitCreator/OutfitForm/OutfitForm.jsx
@@ -50,47 +50,49 @@ export default function OutfitForm() {
     </a>
   </li> 
 </ul>
+</div> 
+    
+    )
+  }
 
-    <div id='form-div'>
-      <form id='outfit-form'>
-        <div className='inputs-div'>
-        <input
+      /* <div id='form-div'>
+        <form id='outfit-form'>
+          <div className='inputs-div'>
+          <input
+            className='form-input'
+            type="text"
+            name="name"
+            placeholder="Outfit name"
+          />
+          <input
+            className='form-input'
+            type="text"
+            name="name"
+            placeholder="Latitude"
+          />
+          <input
           className='form-input'
-          type="text"
-          name="name"
-          placeholder="Outfit name"
-        />
-         <input
-          className='form-input'
-          type="text"
-          name="name"
-          placeholder="Latitude"
-        />
-        <input
-         className='form-input'
-          type="text"
-          name="name"
-          placeholder="Longitude"
-        />
-        </div>
-        <div>
-        <button id='form-btn' type="save">Save Outfit</button>
-        </div>
-        <p href="#">Don't have an account? Sign In</p>
-      </form>
+            type="text"
+            name="name"
+            placeholder="Longitude"
+          />
+          </div>
+          <div>
+          <button id='form-btn' type="save">Save Outfit</button>
+          </div>
+          <p href="#">Don't have an account? Sign In</p>
+        </form>
 
-      
-  <div className="drops">
-    <div className="drop drop-1"></div>
-    <div className="drop drop-2"></div>
-    <div className="drop drop-3"></div>
-    <div className="drop drop-4"></div>
-    <div className="drop drop-5"></div>
-  </div>
+        
+    <div className="drops">
+      <div className="drop drop-1"></div>
+      <div className="drop drop-2"></div>
+      <div className="drop drop-3"></div>
+      <div className="drop drop-4"></div>
+      <div className="drop drop-5"></div>
     </div>
-  </div>
-  )
-}
+  </div>*/
+  
 
       // <form onSubmit={handleSubmit}>
       //   <input
--- src/components/OutfitMap/OutfitMap.jsx
+++ src/components/OutfitMap/OutfitMap.jsx
@@ -17,6 +17,48 @@ const OutfitMap = () => {
                 </Popup>
             </Marker>
             </MapContainer>
+
+<div>
+            <div id='form-div'>
+      <form id='outfit-form'>
+        <div className='inputs-div'>
+        <input
+          className='form-input'
+          type="text"
+          name="name"
+          placeholder="Outfit name"
+        />
+         <input
+          className='form-input'
+          type="text"
+          name="name"
+          placeholder="Latitude"
+        />
+        <input
+         className='form-input'
+          type="text"
+          name="name"
+          placeholder="Longitude"
+        />
+        </div>
+        <div>
+        <button id='form-btn' type="save">Save Outfit</button>
+        </div>
+        <p href="#">Don't have an account? Sign In</p>
+      </form>
+
+      
+  <div className="drops">
+    <div className="drop drop-1"></div>
+    <div className="drop drop-2"></div>
+    <div className="drop drop-3"></div>
+    <div className="drop drop-4"></div>
+    <div className="drop drop-5"></div>
+  </div>
+    </div>
+  </div>
+
+
         </div>
     )
 }
